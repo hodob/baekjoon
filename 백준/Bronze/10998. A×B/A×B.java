0b0bfae6662555a0aa5195fbe2d7709cbd262b95
hodob <90305768+hodob@users.noreply.github.com>
2023-09-25 16:24:58 +0900

    [Bronze V] Title: A×B, Time: 212 ms, Memory: 17684 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main{
+    public static void main(String args[]){
+       // BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Scanner sc = new Scanner(System.in);
+                int a, b;
+        a = sc.nextInt();
+        b = sc.nextInt();
+        System.out.println(a * b);
+    }
+}
