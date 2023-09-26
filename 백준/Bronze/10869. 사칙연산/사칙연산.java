b26ffa1a3c5d4c4fd605d8506b9fb5d0a25d0362
hodob <90305768+hodob@users.noreply.github.com>
2023-09-26 09:38:52 +0900

    [Bronze V] Title: 사칙연산, Time: 124 ms, Memory: 14200 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		new Main().soulution();
+	}
+
+	private void soulution() throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+
+//		double f = Double.parseDouble(st.nextToken());
+//		double f2= Double.parseDouble(st.nextToken());
+		int number1 = Integer.parseInt(st.nextToken());
+		int number2 = Integer.parseInt(st.nextToken());
+		
+		bw.write(String.valueOf(number1+number2));
+		
+		bw.newLine();
+		bw.write(String.valueOf(number1-number2));
+		bw.newLine();
+		bw.write(String.valueOf(number1*number2));
+		bw.newLine();
+		bw.write(String.valueOf(number1/number2));
+		bw.newLine();
+		bw.write(String.valueOf(number1%number2));
+		
+		bw.flush();
+		br.close();
+		bw.close();
+	}
+
+}
