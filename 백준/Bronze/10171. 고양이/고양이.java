bc64ba9f1a62b805072e9228e7ad8e606e13df71
hodob <90305768+hodob@users.noreply.github.com>
2023-09-26 10:26:16 +0900

    [Bronze V] Title: 고양이, Time: 116 ms, Memory: 13904 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,54 @@
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		new Main().soulution();
+	}
+
+	private void soulution() throws Exception {
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+//		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		
+//
+//		String text1 = st.nextToken();
+		sb.append("\\    /\\").append("\n");
+		sb.append(" )  ( ')").append("\n");
+		sb.append("(  /  )").append("\n");
+		sb.append(" \\(__)|").append("\n");
+		System.out.println(sb);
+//
+//
+//		
+		
+//		double f = Double.parseDouble(st.nextToken());
+//		double f2= Double.parseDouble(st.nextToken());
+//		long number1 = Long.parseLong(st.nextToken());
+//		long number2 = Long.parseLong(st.nextToken());
+//		long number3 = Long.parseLong(st.nextToken());
+		
+//		System.out.println(number1+number2+number3);
+//		bw.write(String.valueOf(number1+number2+number3));
+		
+//		bw.newLine();
+//		bw.write(String.valueOf(((number1%number3) + (number2%number3))%number3));
+//		bw.newLine();
+//		bw.write(String.valueOf( (number1*number2)%number3));
+//		bw.newLine();
+//		bw.write(String.valueOf(((number1%number3) * (number2%number3))%number3));
+//		bw.newLine();
+//		bw.write(String.valueOf(number1%number2));
+//		
+//		bw.flush();
+//		br.close();
+//		bw.close();
+	}
+
+}
